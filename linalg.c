0af9fb9d77e70f442c3c27709bac1e7b4ee2c53d
Nicolas Mascot <nm116@aub.edu.lb>
2018-05-06 22:11:48 +0300

    a good part of linalg

--- linalg.c
+++ linalg.c
@@ -52,15 +52,104 @@ GEN Hsort(GEN A, GEN p)
 	return A;
 }
 
-GEN matkerMod(GEN A, GEN p, GEN T, GEN e)
+GEN matkerpadic(GEN A, GEN T, GEN p, long e)
 {
 	pari_sp av;
 	GEN K;
-	if(gequal1(e))
-		return FqM_ker(A,T,p);
+	if(e==1) return FqM_ker(A,T,p);
 	av = avma;
-	K = ZpXQM_ker(A,T,p,itos(e),NULL);
+	K = ZpXQM_ker(A,T,p,e,NULL);
 	return K;
 	K = Hsort(K,p);
 	return gerepilecopy(av,K);
+}
+
+GEN matF(GEN A, GEN T, GEN p, long e)
+{
+	pari_sp av = avma;
+	GEN B;
+	unsigned long r,n,j;
+	r = lg(A);
+	/* reduce A mod p, and supplement */
+	B = FpXT_red(A,p);
+	B = FqM_suppl(B,T,p);
+	/* Un-reduce the A-part */
+	for(j=1;j<r;j++)
+	{
+		gel(B,j) = gel(A,j);
+	}
+	/* invert */
+	B = ZpXQM_inv(A,T,p,e);
+	/* return the first #A rows */
+  n = lg(B);
+	for(j=1;j<n;j++)
+	{
+		setlg(gel(B,j),r);
+	}
+	return gerepilecopy(av,B);
+}
+
+GEN mat2col(GEN A)
+{
+	unsigned long n,m,i,j=1;
+	GEN C;
+	n = lg(A)-1;
+	if(n==0) return cgetg(1,t_COL);
+	m = lg(gel(A,1))-1;
+	C = cgetg(n*m+1,t_COL);
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			gel(C,(i-1)*n+j) = gcoeff(A,i,j);
+		}
+	}
+	return C;
+}
+
+GEN col2mat(GEN C, unsigned long m, unsigned long n)
+{
+	GEN A,Aj;
+	unsigned long i=1,j=1;
+	A = cgetg(n+1,t_MAT);
+	for(j=1;j<=n;j++)
+	{
+		Aj = cgetg(m+1,t_COL);
+		for(i=1;i<=m;i++)
+		{
+			gel(Aj,i) = gel(C,(i-1)*n+j);
+		}
+		gel(A,j) = Aj;
+	}
+	return A;
+}
+
+GEN M2ABCD(GEN M, GEN uv)
+{
+	GEN u,v,res,A,col;
+	unsigned long m,n,i,j,p,q;
+  res = cgetg(5,t_VEC);
+	for(p=1;p<=2;p++)
+	{
+		u = gmael(uv,1,p);
+		m = lg(u);
+		for(q=1;q<=2;q++)
+		{
+			v = gmael(uv,2,q);
+			n = lg(v);
+  		A = cgetg(n,t_MAT);
+			for(j=1;j<n;j++)
+			{
+				col = cgetg(m,t_COL);
+				for(i=1;i<m;i++)
+				{
+					printf("%d %d",u[i],v[j]);
+					gel(col,i) = gcoeff(M,u[i],v[j]);
+				}
+				gel(A,j) = col;
+			}
+			gel(res,q+2*(p-1)) = A;
+		}
+	}
+	return res;
 }
