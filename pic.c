5b702eaf26640f63cb9b6c6f0c77d04efd1eb421
Nicolas Mascot <nm116@aub.edu.lb>
2018-05-16 22:29:07 +0300

    install.gp file

--- pic.c
+++ pic.c
@@ -2,23 +2,23 @@
 #include "pic.h"
 
 ulong Jgetg(GEN J) {return (ulong)gel(J,1);}
-void Jsetg(GEN J, ulong g) {gel(J,1)=g;}
+void Jsetg(GEN J, ulong g) {J[1]=g;}
 ulong Jgetd0(GEN J) {return (ulong)gel(J,2);}
-void Jsetd0(GEN J, ulong d0) {gel(J,2)=d0;}
+void Jsetd0(GEN J, ulong d0) {J[2]=d0;}
 GEN JgetT(GEN J) {return gel(J,3);}
 void JsetT(GEN J, GEN T) {gel(J,3) = T;}
 GEN Jgetp(GEN J) {return gel(J,4);}
 void Jsetp(GEN J, GEN p) {gel(J,4) = p;}
 long Jgete(GEN J) {return (long)gel(J,5);}
-void Jsete(GEN J, ulong e) {gel(J,5)=e;}
+void Jsete(GEN J, ulong e) {J[5]=e;}
 GEN Jgetpe(GEN J) {return gel(J,6);}
 void Jsetpe(GEN J, GEN pe) {gel(J,6)=pe;}
 GEN JgetFrob(GEN J) {return gel(J,7);}
 void JsetFrob(GEN J, GEN Frob) {gel(J,7)=Frob;}
 GEN JgetV(GEN J) {return gel(J,8);}
-void JsetV(GEN J, GEN Frob) {gel(J,8)=V;}
+void JsetV(GEN J, GEN V) {gel(J,8)=V;}
 GEN JgetKV(GEN J) {return gel(J,9);}
-void JsetKV(GEN J, GEN KV) {gel(J,9)=Frob;}
+void JsetKV(GEN J, GEN KV) {gel(J,9)=KV;}
 GEN JgetW0(GEN J) {return gel(J,10);}
 void JsetW0(GEN J, GEN W0) {gel(J,10)=W0;}
 GEN JgetZ(GEN J) {return gel(J,11);}
