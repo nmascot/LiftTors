3497d28757f4e945d1d9b786de1c3c5a3e203aa1
Nicolas Mascot <nm116@aub.edu.lb>
2018-05-17 15:44:33 +0300

    Debug Frob in hyperinit

--- hyper.c
+++ hyper.c
@@ -3,7 +3,7 @@
 
 GEN HyperRandPt(GEN f, GEN T, GEN p, ulong e, GEN pe)
 {
-	pari_sp av2,av = avma;
+	pari_sp av = avma;
 	long vT,dT;
 	GEN x,y2,y,P;
 
@@ -13,15 +13,13 @@ GEN HyperRandPt(GEN f, GEN T, GEN p, ulong e, GEN pe)
 	{
 		avma = av;
 		x = random_FpX(dT-1,vT,p);
-		av2 = avma;
 		y2 = poleval(f,x);
 		y2 = FpXQ_red(y2,T,pe);
 		if(gequal0(FpXQ_red(y2,T,p))) continue;
 		y = ZpXQ_sqrt(y2,T,p,e);
 		if(y == NULL) continue;
-    y = gerepileupto(av2,y);
 		P = mkvec2(x,y);
-		return P;
+		return gerepilecopy(av,P);
 	}
 }
 
@@ -72,55 +70,69 @@ GEN HyperInit(GEN f, GEN p, ulong a, ulong e)
 	g = df/2-1;
 	d0 = df;
 	nZ = 6*d0+1;
+	printf("A");
 
 	t = varlower("t",varn(f));
   T = liftint(ffinit(p,a,varn(t)));
 	Frob = ZpX_Frobenius(T,p,e);
 	pe = powiu(p,e);
+	printf("B");
+
 	
 	n = ncyc = 0;
 	Z = cgetg(nZ+a,t_VEC);
 	Zp = cgetg(nZ+a,t_VEC);
-	FrobCyc = cgetg(nZ,t_VECSMALL);
+	FrobCyc = cgetg(nZ+1,t_VECSMALL);
 	while(n<nZ)
 	{
+		printf("Loop, n=%lu\n",n);
 		avP = avma;
 		P = HyperRandPt(f,T,p,e,pe);
+		pari_printf("%Ps\n",P);
 		/* Already have it ? */
 		Pp = FpXV_red(P,p);
 	  newpt = 1;
 		for(i=1;i<=n;i++)
 		{
+			printf("Verif doublon %lu\n",i);
 			if(gequal(Pp,gel(Zp,i)))
 			{
 				newpt = 0;
-				av = avP;
+				printf("Notnew");
+				avma = avP;
 				break;
 			}
 		}
+		printf("End verif\n");
 		if(newpt == 0) continue;
 		ncyc++;
 		Q = P;
 		i = 0;
+		printf("Starting cycle %lu\n",ncyc);
 		do
 		{
 			i++;
 			n++;
+			printf("Point %lu, step %lu\n",n,i);
+			pari_printf("%Ps\n",Q);
 			gel(Z,n) = Q;
 			gel(Zp,n) = FpXV_red(Q,p);
-			x = gel(Q,1); y = gel(Q,2);
-			x = FpXQ_red(poleval(x,Frob),T,pe);
-			y = FpXQ_red(poleval(y,Frob),T,pe);
+			printf("Now apply Frob\n");
+			x = FpX_FpXQ_eval(gel(Q,1),Frob,T,pe);
+			y = FpX_FpXQ_eval(gel(Q,2),Frob,T,pe);
 			Q = mkvec2(x,y);
 		} while(!gequal(Q,P));
     FrobCyc[ncyc] = i;
+		printf("Cycle %lu has length %lu\n",ncyc,i);
 	}
 	setlg(Z,n+1);
 	setlg(FrobCyc,ncyc+1);
+	printf("D");
 
 	W0 = RReval(Z,d0,df,T,pe);
 	V = RReval(Z,3*d0/2,df,T,pe);
 	KV = shallowtrans(matkerpadic(shallowtrans(V),T,p,e));
+	printf("E");
 
 	J = cgetg(lgJ,t_VEC);
 	Jsetg(J,g);
@@ -135,6 +147,8 @@ GEN HyperInit(GEN f, GEN p, ulong a, ulong e)
 	JsetW0(J,W0);
 	JsetZ(J,Z);
 	JsetFrobCyc(J,FrobCyc);
+	printf("F");
 
+	return J;
 	return gerepilecopy(av,J);
 }
