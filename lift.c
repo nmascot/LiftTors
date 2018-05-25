f3e190de02ad89d57774567b6a6bd93a184d8e93
Nicolas Mascot <nm116@aub.edu.lb>
2018-05-25 16:56:13 +0300

    Fix bugs in lift

--- lift.c
+++ lift.c
@@ -7,12 +7,16 @@ GEN PicLiftTors_2(GEN J2, GEN W1, ulong e1, GEN l)
 {
 	pari_sp av1,av=avma;
 	GEN V,KV,T,p,pe1,pe2,pe21;
-	GEN K,wV,KwV,Ainv,AinvB,CAinv,rho,ABCD,uv;
+	GEN col,K,wV,KwV,Ainv,AinvB,CAinv,rho,ABCD,uv;
 	ulong g,d0,nW,nV,nKV,nZ,e2,r;
 	ulong e,j,P;
 
-
-	/*TODO test e2>e1 */
+	e2 = Jgete(J2);
+	pe2 = Jgetpe(J2);
+	if(e2<=e1)
+	{
+		return FpXM_red(W1,pe2);
+	}
 
 	V = JgetV(J2);
 	KV = JgetKV(J2);
@@ -25,23 +29,24 @@ GEN PicLiftTors_2(GEN J2, GEN W1, ulong e1, GEN l)
 	T = JgetT(J2);
 	p = Jgetp(J2);
 	pe1 = powiu(p,e1);
-	e2 = Jgete(J2);
-	pe2 = Jgetpe(J2);
 	pe21=powiu(p,e2-e1);
+	printf("a");
 
 	av1 = avma;
 
-	wV = cgetg(nW+1,t_MAT);
-	for(j=1;j<=nW;j++)
+	wV = cgetg(nV+1,t_MAT);
+	for(j=1;j<=nV;j++)
 	{
-		gel(wV,j) = cgetg(nZ+1,t_COL);
+		col = cgetg(nZ+1,t_COL);
 		for(P=1;P<=nZ;P++)
 		{
-			gcoeff(wV,P,j) = Fq_mul(gcoeff(W1,P,1),gcoeff(V,P,j),T,pe2);
+			gel(col,P) = Fq_mul(gcoeff(W1,P,1),gcoeff(V,P,j),T,pe2);
 		}
+		gel(wV,j) = col;
 	}
 	KwV = mateqnpadic(wV,T,p,e2);
 	KwV = gerepileupto(av1,KwV);
+	printf("b");
 
 	av1 = avma;
 	K = cgetg(nZ+1,t_MAT);
@@ -67,17 +72,14 @@ GEN PicLiftTors_2(GEN J2, GEN W1, ulong e1, GEN l)
 		}
 	}
 	r = nZ-(d0+1-g);
-	uv = FqM_MinorCompl(K,T,p); /* TODO */
+	uv = FqM_MinorCompl(K,T,p);
+	printf("d");
 	ABCD = M2ABCD(K,uv);
 	Ainv = ZpXQM_inv(gel(ABCD,1),T,p,e2);
 	CAinv = FqM_mul(gel(ABCD,3),Ainv,T,pe2);
 	AinvB = FqM_mul(Ainv,gel(ABCD,2),T,pe2);
 	rho = FqM_mul(CAinv,gel(ABCD,2),T,pe2);
+	printf("e");
 	rho = RgM_sub(gel(ABCD,4),rho);
-	/*for(j=1;j<=nZ-r;j++)
-	{
-		for(i=1;i<=nW*nKV;i++)
-		{
-	*/
-	return gerepileupto(av,rho);
+
 }
